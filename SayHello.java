3084b9eb2c77f2068ef942bf14b3f99730d80ef0
KASHlMA <gustavoanayalopez32@gmail.com>
2025-05-05 23:29:12 -0600

    Inserte otro println con un como estas

--- SayHello.java
+++ SayHello.java
@@ -2,8 +2,8 @@ public class SayHello{
     public class static void main(String args[]){
         System.out.println("Hola");
         System.out.println("Gustavo");
-
         System.out.println("18");
+        System.out.println("Como estas");
 
     }
 
